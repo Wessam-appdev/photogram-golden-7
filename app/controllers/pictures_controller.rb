8937aab8992a8e7aed2fa6d765914687e769499b
Wessam Abozeid <wessam@uchicago.edu>
2018-10-28 17:30:28 +0000

    3rd
    homepage left

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -18,26 +18,53 @@ class PicturesController < ApplicationController
   
   #READ
   def index
+    @photo_list=Photo.all
     render("/read/all_photos.html.erb")
   end
   
   def show
+    
+    @the_id=params.fetch("the_id")
+    the_photo=Photo.find(@the_id)
+    
+    
+    @photo_source= the_photo.source
+    @photo_caption=the_photo.caption
+    # @photo_ago= time_ago_in_words(the_photo.created_at)
     render("/read/single_photo.html.erb")
   end
   
   #UPDATE
   
   def edit_form
+    @edit_id=params.fetch("an_id")
+    the_photo=Photo.find(@edit_id)
+    @old_source=the_photo.source
+    @old_caption=the_photo.caption
+    
+    
     render("/update/photo_update_form.html.erb")
   end
   
   def update_row
-    render("/update/photo_update_result.html.erb")
+    
+    update_id=params.fetch("some_id")
+    photo=Photo.find(update_id)
+    photo.source=params.fetch("user_url")
+    photo.caption=params.fetch("user_caption")
+    photo.save
+    
+    
+    redirect_to("/photos/"+update_id)
   end
   
   #DELETE
   
   def destroy_row
+    the_id=params.fetch("toast_id")
+    photo=Photo.find(the_id)
+    photo.destroy
+    
     render("/delete/delete_result.html.erb")
   end
 
